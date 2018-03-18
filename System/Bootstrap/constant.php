8ed6a7708d65973b52ba488dfac59b9da5686b75
down <1960754013@qq.com>
2018-03-18 23:13:53 +0800

    20180318 23:00

--- System/Bootstrap/constant.php
+++ System/Bootstrap/constant.php
@@ -4,12 +4,16 @@ define('EXT', '.php');
 
 define('SP', DIRECTORY_SEPARATOR);
 
+defined('ROOT_PATH') or define('ROOT_PATH', dirname(dirname(__DIR__)) . SP);
+
 defined('SYSTEM_PATH') or define('SYSTEM_PATH', dirname(__DIR__). SP );
 
 define('CORE_PATH', SYSTEM_PATH . 'Core' . SP);
 
-defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . SP);
+defined('APP_PATH') or define('APP_PATH', ROOT_PATH . 'Application' . SP);
 
-defined('ROOT_PATH') or define('ROOT_PATH', dirname(dirname(__DIR__)) . SP);
+defined('VIEW_PATH') or define('VIEW_PATH', APP_PATH . SP);
+
+defined('ENVIRONMENT') or define('ENVIRONMENT','development');
 
 defined("DEBUG") or define("DEBUG",true);
